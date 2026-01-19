8a89eb0732d2ca79bf6e7a26faa86c510ec07d16
cayeyemito <cayetanoym@gmail.com>
2026-01-19 12:04:28 +0100

    Prueba

--- ifphub/src/app/citas/page.tsx
+++ ifphub/src/app/citas/page.tsx
@@ -38,7 +38,7 @@ export default function Page() {
     <SidebarProvider>
       <AppSidebar uid={uid} sig={sig}/>
 
-      <SidebarInset>
+      <SidebarInset>º
 
         {/* HEADER */}
         <header className="flex h-16 shrink-0 items-center gap-2 border-b border-gray-200">
